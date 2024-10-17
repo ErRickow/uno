5cfb349a9c7a2e7f498dba82d2f7bf34cd97b7ba
Just R <remixingmagelang@gmail.com>
2024-10-18 00:55:57 +0700

    update internationalization.py

--- internationalization.py
+++ internationalization.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# Telegram bot to play UNO in group chats
-# Copyright (c) 2016 Jannes Höke <uno@jhoeke.de>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
 import gettext
 from functools import wraps
 
@@ -107,7 +87,7 @@ def user_locale(func):
         with db_session:
             us = UserSetting.get(id=user.id)
 
-        if us and us.lang != 'id':
+        if us and us.lang != 'en':
             _.push(us.lang)
         else:
             _.push('en_US')
@@ -130,7 +110,7 @@ def game_locales(func):
             for player in player.game.players:
                 us = UserSetting.get(id=player.user.id)
 
-                if us and us.lang != 'id':
+                if us and us.lang != 'en':
                     loc = us.lang
                 else:
                     loc = 'en_US'
